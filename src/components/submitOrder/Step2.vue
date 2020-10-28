60c534591a7bdae83ff99191b682c88877243765
Thomas Bricker <thhomasbricker87@gmail.com>
2020-10-28 09:27:45 +0200

    data for step 2

---
+++ src/components/submitOrder/Step2.vue
@@ -0,0 +1,48 @@
+<template>
+<div>
+  
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Step1',
+
+  props: ['', ''],
+
+  data: () => ({
+    // Technical details
+    fullSiteAddress: '',
+    serviceDescription: '',
+    vlan: '',
+    aEnd: '',
+    customerName: '',
+    zEnd: '',
+    // buildings
+    name: '',
+    company: '',
+    emailAdress: '',
+    position: '',
+    contactNumber: '',
+    //  End customers
+    companyName: '',
+    position2: '',
+    emailAdress2: '',
+    siteContactName: '',
+    contactNumber2: '',
+
+    deposit: false,
+    bankGuarantee: false,
+    generalSequrityAgreement: false,
+    noneRequired: false,
+  }),
+  computed: {
+    //
+  },
+
+}
+</script>
+
+<style>
+
+</style>
