ab632af2fdc65884dd3c2d2ab7af7b627b9d52bb
Thomas Bricker <thomasbricker87@gmail.com>
2020-12-20 16:17:44 +0200

    clear site field

--- src/components/popUps/SignUp.vue
+++ src/components/popUps/SignUp.vue
@@ -140,7 +140,7 @@ export default {
       phone: '',
       email: '',
       additionalEmail: '',
-      site: 'https://www.wikipedia.org/',
+      site: '',
       login: '',
       password: '',
       company: '',
