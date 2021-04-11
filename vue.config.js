3e6a6d2ac5d5884e83f15d0a1c6e167b24eee0ce
Irina Fylyppova <irina.h.fylyppova@gmail.com>
2021-04-11 17:33:45 +0300

    public path for github

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  runtimeCompiler: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/dgtek-portal/'
+    : '/'
 }
